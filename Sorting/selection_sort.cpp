3a482126a0932c8df1f11889caade73e45a1e79a
Mst. Israt Jahan Shefa <isratjahanshefa506@gmail.com>
2025-05-27 03:34:44 +0600

    Update selection_sort.cpp

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,36 +1,47 @@
+//selection sort
 #include <bits/stdc++.h>
 using namespace std;
 
-void selectionSort(int arr[], int size){
-    for (int i = 0; i < size - 1;i++){
-        int selectionIndex = i;
-        for (int j = i + 1; j < size;j++){
-            if (arr[j]< arr[selectionIndex]){
-                selectionIndex = j;
-            }
-        }
-        swap(arr[i], arr[selectionIndex]);
-    }
-}
-
-void printArray(int arr[], int size){
-    for (int i = 0; i < size;i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+void selection_sort(int size, int arr[]);
 
 int main(){
     int size;
-    printf("Enter array size:");
+    cout << "Enter array size: ";
     cin >> size;
+
     int arr[size];
-    printf("Enter array elements:");
-    for (int i = 0; i < size; i++) {
+    cout << "Enter Array elements: ";
+    for(int i=0;i<size;i++){
         cin >> arr[i];
     }
-    selectionSort(arr, size);
-    printf("Sorted array:");
-    printArray(arr, size);
+
+    cout << "Original array is: ";
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selection_sort(size, arr);
+
+    cout << "After selection sort the array is: ";
+    for(int i=0;i<size;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
+}
+
+void selection_sort(int size, int arr[]){
+    for(int i=0; i<size-1;i++){
+        int smallestIndex=i;
+        for(int j=i+1; j<size;j++){
+            if(arr[j]<arr[smallestIndex]){
+                smallestIndex=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[smallestIndex];
+        arr[smallestIndex]=temp;
+    }
 }
